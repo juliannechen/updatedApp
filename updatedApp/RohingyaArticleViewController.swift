8bff49eb39bc4b3f41b0669f580bb53daaacd506
Julianne <julianneychen@gmail.com>
2020-07-16 10:11:01 -0700

    add scrollView

---
+++ updatedApp/RohingyaArticleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RohingyaArticleViewController.swift
+//  updatedApp
+//
+//  Created by Julianne on 7/15/20.
+//  Copyright © 2020 Julianne Chen. All rights reserved.
+//
+
+import UIKit
+
+class RohingyaArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
